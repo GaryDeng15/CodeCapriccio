63bf07148e937b90415f4405a53b14a5ca05ae7f
Gary <150769273+GaryDeng15@users.noreply.github.com>
2024-11-14 23:03:54 +0800

    Eposide08_反转链表_双指针写法

---
+++ LinkedList/T206/ReverseList.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+};
+
+ListNode* reverseList(ListNode* head) {
+	if (head != nullptr && head->next != nullptr) {
+		ListNode* pre = nullptr;
+		ListNode* cur = head;
+		while (cur) {
+			ListNode* temp = new ListNode();
+			temp = cur->next;
+			cur->next = pre;
+			pre = cur;
+			cur = temp;
+		}
+		head = pre;
+	}
+	return head;
+}
+
+int main() {
+
+	/**
+	* 示例 1：
+		输入：head = [1,2,3,4,5]
+		输出：[5,4,3,2,1]
+	*/
+	
+	/**
+	* 示例 2：
+		输入：head = [1,2]
+		输出：[2,1]
+	*/
+
+	system("pause");
+	return 0;
+}
