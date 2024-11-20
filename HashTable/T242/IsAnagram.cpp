712430716e45d9504771bf548c67383b17343a83
Gary <150769273+GaryDeng15@users.noreply.github.com>
2024-11-20 15:34:53 +0800

    Episode12_有效字母异位词
    哈希数组解法。

--- HashTable/T242/IsAnagram.cpp
+++ HashTable/T242/IsAnagram.cpp
@@ -4,7 +4,19 @@
 using namespace std;
 
 bool isAnagram(string s, string t) {
+	int charRecord[26] = {0};
+	for (int i = 0; i < s.size(); i++) {
+		charRecord[s[i] - 'a']++;
+	}
 
+	for (int j = 0; j < t.size(); j++) {
+		charRecord[t[j] - 'a']--;
+	}
+
+	for (int index = 0; index < 26; index++) {
+		if (charRecord[index] != 0) return false;
+	}
+	return true;
 }
 
 int main() {
