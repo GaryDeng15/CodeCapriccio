53b1fdc91a543360b1d79c34201d5f4fabc45ff4
Gary <150769273+GaryDeng15@users.noreply.github.com>
2024-11-23 11:20:20 +0800

    Episode15_四数相加优化写法

--- HashTable/T454/FourSum.cpp
+++ HashTable/T454/FourSum.cpp
@@ -5,28 +5,17 @@
 using namespace std;
 
 int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
-	int iterCount = nums1.size();
 	unordered_map<int, int> record_map;
 	int result = 0;
-	for (int i = 0; i < iterCount; i++) {
-		for (int j = 0; j < iterCount; j++) {
-			int tempSum = nums1[i] + nums2[j];
-			if (record_map.find(tempSum) != record_map.end()) {
-				int count = record_map[tempSum];
-				record_map.insert(pair<int,int>(tempSum, ++count));
-			}
-			else {
-				record_map.insert(pair<int, int>(tempSum, 1));
-			}
+	for (int a : nums1) {
+		for (int b : nums2) {
+			record_map[a + b]++;
 		}
 	}
 
-	for (int k = 0; k < iterCount; k++) {
-		for (int l = 0; l < iterCount; l++) {
-			int tempSum = nums3[k] + nums4[l];
-			if (record_map.find(0 - tempSum) != record_map.end()) {
-				result += record_map[0 - tempSum];
-			}
+	for (int c : nums3) {
+		for (int d : nums4) {
+			result += record_map[0 - (c + d)];
 		}
 	}
 
@@ -53,15 +42,15 @@ int main() {
 		Êä³ö£º1
 	*/
 
-	/*vector<int> nums1 = {1, 2};
+	vector<int> nums1 = {1, 2};
 	vector<int> nums2 = {-2, -1};
 	vector<int> nums3 = {-1, 2};
-	vector<int> nums4 = {0, 2};*/
+	vector<int> nums4 = {0, 2};
 
-	vector<int> nums1 = { 0 };
+	/*vector<int> nums1 = { 0 };
 	vector<int> nums2 = { 0 };
 	vector<int> nums3 = { 0 };
-	vector<int> nums4 = { 0 };
+	vector<int> nums4 = { 0 };*/
 
 	int result = fourSumCount(nums1, nums2, nums3, nums4);
 	cout << "Result is " << result << endl;
