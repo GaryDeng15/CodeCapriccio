b8199eb5308d63c3aee971e615024dc9c00f4af5
Gary <GaryDeng15@users.noreply.github.com>
2024-11-25 23:46:12 +0800

    Episode16_三数之和 哈希法

--- HashTable/T15/ThreeSum.cpp
+++ HashTable/T15/ThreeSum.cpp
@@ -1,10 +1,40 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
+#include<unordered_set>
 
 using namespace std;
 
 vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> result;
+    sort(nums.begin(), nums.end());
 
+    for (int i = 0; i < nums.size(); i++) {
+        
+        if (nums[i] > 0) {
+            continue;
+        }
+        if (i > 0 && nums[i] == nums[i - 1]) { 
+            continue;
+        }
+        unordered_set<int> set;
+        for (int j = i + 1; j < nums.size(); j++) {
+            if (j > i + 2
+                && nums[j] == nums[j - 1]
+                && nums[j - 1] == nums[j - 2]) { 
+                continue;
+            }
+            int c = 0 - (nums[i] + nums[j]);
+            if (set.find(c) != set.end()) {
+                result.push_back({ nums[i], nums[j], c });
+                set.erase(c);
+            }
+            else {
+                set.insert(nums[j]);
+            }
+        }
+    }
+    return result;
 }
 
 int main() {
@@ -35,7 +65,6 @@ int main() {
 		解释：唯一可能的三元组和为 0 。
  
 		提示：
-
 		3 <= nums.length <= 3000
 		-105 <= nums[i] <= 105
 	*/
