143e26ad0d0e5b1f003753c18d0ad49a9dd9b90c
Gary <GaryDeng15@users.noreply.github.com>
2024-12-20 10:34:49 +0800

    Episode32_T94_二叉树中序遍历 迭代遍历

--- BinaryTree/T94/InorderTraversal.cpp
+++ BinaryTree/T94/InorderTraversal.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<stack>
 
 using namespace std;
 
@@ -26,6 +27,25 @@ public:
 	vector<int> inorderTraversal(TreeNode* root) {
 		vector<int> result;
 		traversal(root, result);
+		return result;
+	}
+
+	vector<int> inorderTraversal(TreeNode* root) {
+		vector<int> result;
+		stack<TreeNode*> st;
+		TreeNode* cur = root;
+		while (cur != NULL || !st.empty()) {
+			if (cur != NULL) { // 指针来访问节点，访问到最底层
+				st.push(cur); // 将访问的节点放进栈
+				cur = cur->left;                // 左
+			}
+			else {
+				cur = st.top(); // 从栈里弹出的数据，就是要处理的数据（放进result数组里的数据）
+				st.pop();
+				result.push_back(cur->val);     // 中
+				cur = cur->right;               // 右
+			}
+		}
 		return result;
 	}
 };
