a0d0ea41d9a07e9db4feb3315caf26789c651acf
Gary <GaryDeng15@users.noreply.github.com>
2025-01-05 17:48:02 +0800

    Episode32_T144_二叉树后序遍历                 统一迭代遍历

--- BinaryTree/T145/PostorderTraversal.cpp
+++ BinaryTree/T145/PostorderTraversal.cpp
@@ -46,6 +46,32 @@ public:
 		reverse(result.begin(), result.end()); // 将结果反转之后就是左右中的顺序了
 		return result;
 	}
+
+	// 统一迭代遍历
+	vector<int> postorderTraversal(TreeNode* root) {
+		vector<int> result;
+		stack<TreeNode*> st;
+		if (root != NULL) st.push(root);
+		while (!st.empty()) {
+			TreeNode* node = st.top();
+			if (node != NULL) {
+				st.pop();
+				st.push(node);                          // 中
+				st.push(NULL);
+
+				if (node->right) st.push(node->right);  // 右
+				if (node->left) st.push(node->left);    // 左
+
+			}
+			else {
+				st.pop();
+				node = st.top();
+				st.pop();
+				result.push_back(node->val);
+			}
+		}
+		return result;
+	}
 };
 
 int main() {
