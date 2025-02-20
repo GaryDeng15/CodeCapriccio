e3e1374f2a8c36e5887cca97460de82647b30259
Gary <GaryDeng15@users.noreply.github.com>
2025-02-20 20:38:01 +0800

    Episode45_T112_路径总和

---
+++ BinaryTree/T112/PathSum.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+private:
+    bool traversal(TreeNode* cur, int count) {
+        if (!cur->left && !cur->right && count == 0) return true; 
+        if (!cur->left && !cur->right) return false; 
+
+        if (cur->left) { 
+            count -= cur->left->val; 
+            if (traversal(cur->left, count)) return true;
+            count += cur->left->val; 
+        }
+        if (cur->right) { 
+            count -= cur->right->val; 
+            if (traversal(cur->right, count)) return true;
+            count += cur->right->val; 
+        }
+        return false;
+    }
+
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if (root == NULL) return false;
+        return traversal(root, sum - root->val);
+    }
+};
+
+int main() {
+
+    system("pause");
+    return 0;
+}
