9c1ecdbe8ba8c493e8ecbb37260f1df8af55babd
Gary <GaryDeng15@users.noreply.github.com>
2025-02-26 16:42:16 +0800

    Episode48_T617_合并二叉树/n后序

--- BinaryTree/T617/MergeTrees.cpp
+++ BinaryTree/T617/MergeTrees.cpp
@@ -33,6 +33,15 @@ public:
         t1->right = mergeTrees_midOrder(t1->right, t2->right); 
         return t1;
     }
+
+    TreeNode* mergeTrees_postOrder(TreeNode* t1, TreeNode* t2) {
+        if (t1 == NULL) return t2; 
+        if (t2 == NULL) return t1; 
+        t1->left = mergeTrees(t1->left, t2->left);      
+        t1->right = mergeTrees(t1->right, t2->right);   
+        t1->val += t2->val;                             
+        return t1;
+    }
 };
 
 int main() {
