c2f99f826614cd7982bf154c2c40f6a918a9708d
Gary <GaryDeng15@users.noreply.github.com>
2025-03-12 14:47:31 +0800

    Episode44_T513_找树左下角的值_迭代法

--- BinaryTree/T513/FindBottomLeftValue.cpp
+++ BinaryTree/T513/FindBottomLeftValue.cpp
@@ -16,6 +16,7 @@ struct TreeNode {
 
 class Solution {
 public:
+    // 递归法
     int maxDepth = INT_MIN;
     int result;
     void traversal(TreeNode* root, int depth) {
@@ -40,6 +41,24 @@ public:
     }
     int findBottomLeftValue(TreeNode* root) {
         traversal(root, 0);
+        return result;
+    }
+
+    // 迭代法
+    int findBottomLeftValue(TreeNode* root) {
+        queue<TreeNode*> que;
+        if (root != NULL) que.push(root);
+        int result = 0;
+        while (!que.empty()) {
+            int size = que.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = que.front();
+                que.pop();
+                if (i == 0) result = node->val; // 记录最后一行第一个元素
+                if (node->left) que.push(node->left);
+                if (node->right) que.push(node->right);
+            }
+        }
         return result;
     }
 };
