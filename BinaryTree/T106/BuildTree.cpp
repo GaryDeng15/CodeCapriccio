5da6bf82279da6b9dd1426a1711ea3d59ed541c1
Gary <GaryDeng15@users.noreply.github.com>
2025-03-17 20:46:24 +0800

    Episode46_T106_从中序与后序遍历序列构造二叉树

---
+++ BinaryTree/T106/BuildTree.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<algorithm>
+
+using namespace std;
+
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+private:
+    TreeNode* traversal(vector<int>& inorder, vector<int>& postorder) {
+        if (postorder.size() == 0) return NULL;
+
+        // 最后一个元素
+        int rootValue = postorder[postorder.size() - 1];
+        TreeNode* root = new TreeNode(rootValue);
+
+        if (postorder.size() == 1) return root;
+
+        int delimiterIndex;
+        for (delimiterIndex = 0; delimiterIndex < inorder.size(); delimiterIndex++) {
+            if (inorder[delimiterIndex] == rootValue) break;
+        }
+
+        // [0, delimiterIndex)
+        vector<int> leftInorder(inorder.begin(), inorder.begin() + delimiterIndex);
+        // [delimiterIndex + 1, end)
+        vector<int> rightInorder(inorder.begin() + delimiterIndex + 1, inorder.end());
+
+        postorder.resize(postorder.size() - 1);
+
+        // [0, leftInorder.size)
+        vector<int> leftPostorder(postorder.begin(), postorder.begin() + leftInorder.size());
+        // [leftInorder.size(), end)
+        vector<int> rightPostorder(postorder.begin() + leftInorder.size(), postorder.end());
+
+        root->left = traversal(leftInorder, leftPostorder);
+        root->right = traversal(rightInorder, rightPostorder);
+
+        return root;
+    }
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        if (inorder.size() == 0 || postorder.size() == 0) return NULL;
+        return traversal(inorder, postorder);
+    }
+};
+
+
+int main() {
+
+    system("pause");
+    return 0;
+}
