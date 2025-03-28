2f0185f0f46794c8f78313300e535d969c29d75e
Gary <GaryDeng15@users.noreply.github.com>
2025-03-28 17:52:38 +0800

    Episode56_T108_将有序数组转为二叉搜索树_迭代法

--- BinaryTree/T108/SortedArrayToBST.cpp
+++ BinaryTree/T108/SortedArrayToBST.cpp
@@ -17,6 +17,7 @@ struct TreeNode {
 
 class Solution {
 private:
+    // 递归法
     TreeNode* traversal(vector<int>& nums, int left, int right) {
         if (left > right) return nullptr;
         int mid = left + ((right - left) / 2);
@@ -28,6 +29,44 @@ private:
 public:
     TreeNode* sortedArrayToBST(vector<int>& nums) {
         TreeNode* root = traversal(nums, 0, nums.size() - 1);
+        return root;
+    }
+
+    // 迭代法
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        if (nums.size() == 0) return nullptr;
+
+        TreeNode* root = new TreeNode(0);   
+        queue<TreeNode*> nodeQue;           
+        queue<int> leftQue;                 
+        queue<int> rightQue;                
+        nodeQue.push(root);                 
+        leftQue.push(0);                    
+        rightQue.push(nums.size() - 1);     
+
+        while (!nodeQue.empty()) {
+            TreeNode* curNode = nodeQue.front();
+            nodeQue.pop();
+            int left = leftQue.front(); leftQue.pop();
+            int right = rightQue.front(); rightQue.pop();
+            int mid = left + ((right - left) / 2);
+
+            curNode->val = nums[mid];       // 将mid对应的元素给中间节点
+
+            if (left <= mid - 1) {          // 处理左区间
+                curNode->left = new TreeNode(0);
+                nodeQue.push(curNode->left);
+                leftQue.push(left);
+                rightQue.push(mid - 1);
+            }
+
+            if (right >= mid + 1) {         // 处理右区间
+                curNode->right = new TreeNode(0);
+                nodeQue.push(curNode->right);
+                leftQue.push(mid + 1);
+                rightQue.push(right);
+            }
+        }
         return root;
     }
 };
