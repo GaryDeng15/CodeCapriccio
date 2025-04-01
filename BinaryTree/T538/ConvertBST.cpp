7a605807474e8cd39a6e7aa200eb5c61753f7396
Gary <GaryDeng15@users.noreply.github.com>
2025-04-01 20:01:41 +0800

    Episode58_T538_将二叉搜索树转换成累加树_迭代法

--- BinaryTree/T538/ConvertBST.cpp
+++ BinaryTree/T538/ConvertBST.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<stack>
 
 using namespace std;
 
@@ -24,6 +25,33 @@ private:
         cur->val += pre;
         pre = cur->val;
         traversal(cur->left);
+    }
+public:
+    TreeNode* convertBST(TreeNode* root) {
+        pre = 0;
+        traversal(root);
+        return root;
+    }
+
+// 迭代法
+private:
+    int pre; // 记录前一个节点的数值
+    void traversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        TreeNode* cur = root;
+        while (cur != NULL || !st.empty()) {
+            if (cur != NULL) {
+                st.push(cur);
+                cur = cur->right;   // 右
+            }
+            else {
+                cur = st.top();     // 中
+                st.pop();
+                cur->val += pre;
+                pre = cur->val;
+                cur = cur->left;    // 左
+            }
+        }
     }
 public:
     TreeNode* convertBST(TreeNode* root) {
